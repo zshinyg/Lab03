7e2e1ced78132fc5995799e9eec630a7e0040299
Coltyn Gatton <c527g802@ku.edu>
2018-02-12 15:45:28 -0600

    Added password function

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,4 +1,32 @@
 /**
+ * @file passwordValidator.js
  * @author Coltyn Gatton
- * @description 
+ * @description allows a user to enter a password twice in order to validate it.
  */
+
+/**
+ * This function compares the two passwords to eachother.
+ */
+function validatePassword(){
+    
+
+
+    var password = document.getElementById("password").value;
+    var passwordValidation = document.getElementById("passwordValidate").value;
+
+
+    console.log(password);
+    console.log(passwordValidate);
+    if( password == passwordValidation){
+        alert("Success! your password has been validated");
+        return;
+    }
+    else if( password.length < 8 ){
+        alert("Password must be 8 or more characters.");
+        return;
+    }
+    else{
+        alert("Passwords do not match. Please enter them again.");
+        return;
+    }
+}
